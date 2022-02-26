2afa5a6e150fd5c83fd83a59d64589894dc91489
mrintro <aniketpanwar.1998@gmail.com>
2022-02-26 16:43:40 +0530

    Adapter added

--- app/src/main/java/com/example/moviesinshorts/adapter/MovieListAdapter.java
+++ app/src/main/java/com/example/moviesinshorts/adapter/MovieListAdapter.java
@@ -1,12 +1,19 @@
 package com.example.moviesinshorts.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.moviesinshorts.R;
 import com.example.moviesinshorts.model.MovieModel;
 
 import java.util.List;
@@ -21,26 +28,40 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyVi
         this.movieList = movieList;
     }
 
+    public void setMovieList(List<MovieModel> movieList) {
+        this.movieList = movieList;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public MovieListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_row, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieListAdapter.MyViewHolder holder, int position) {
+        holder.tvTitle.setText(this.movieList.get(position).getTitle());
+        Glide.with(context).load(this.movieList.get(position).getPoster_path()).apply(RequestOptions.centerCropTransform()).into(holder.imageView);
 
     }
 
     @Override
     public int getItemCount() {
+        if(this.movieList!=null) return movieList.size();
         return 0;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        ImageView imageView;
+
         public MyViewHolder(View viewItem) {
             super(viewItem);
-
+            tvTitle = (TextView)viewItem.findViewById(R.id.titleView);
+            imageView = (ImageView)viewItem.findViewById(R.id.imageView);
         }
     }
 
--- app/src/main/java/com/example/moviesinshorts/movieFragment/MovieFragment.java
+++ app/src/main/java/com/example/moviesinshorts/movieFragment/MovieFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,10 +18,18 @@ import android.view.ViewGroup;
 import com.example.moviesinshorts.R;
 import com.example.moviesinshorts.adapter.MovieListAdapter;
 import com.example.moviesinshorts.databinding.FragmentMovieBinding;
+import com.example.moviesinshorts.model.MovieModel;
+import com.example.moviesinshorts.viewmodel.MovieListViewModel;
+
+import java.util.List;
 
 public class MovieFragment extends Fragment {
 
     private FragmentMovieBinding fragmentMovieBinding;
+
+    private List<MovieModel> movieList;
+    private MovieListAdapter adapter;
+    private MovieListViewModel viewModel;
 
     public MovieFragment() {
         // Required empty public constructor
@@ -37,8 +48,18 @@ public class MovieFragment extends Fragment {
         RecyclerView recyclerView = movieFragmentView.findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new GridLayoutManager(this.getActivity(),2);
         recyclerView.setLayoutManager(layoutManager);
-        MovieListAdapter adapter = new MovieListAdapter();
+        adapter = new MovieListAdapter(getContext(),movieList);
         recyclerView.setAdapter(adapter);
+
+        viewModel = ViewModelProviders.of(getActivity()).get(MovieListViewModel.class);
+        viewModel.getNowPlayingLiveData().observe(getActivity(), new Observer<List<MovieModel>>() {
+            @Override
+            public void onChanged(List<MovieModel> movieModels) {
+                if(movieModels!=null){
+                    movieList = movieModels;
+                }
+            }
+        });
 
         return movieFragmentView;
     }
